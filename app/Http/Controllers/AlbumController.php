b80e9a38c0665bb97c42aef17c2e4901a49a8d16
Tacettin Kustu <tacettinkustu@gmail.com>
2022-02-19 23:21:02 +0300

    Complete CRUD request changes

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -15,7 +15,7 @@ class AlbumController extends Controller
      */
     public function index()
     {
-        //
+        return Album::all();
     }
 
     /**
@@ -38,7 +38,7 @@ class AlbumController extends Controller
      */
     public function show(Album $album)
     {
-        //
+        return $album;
     }
 
     /**
@@ -50,7 +50,9 @@ class AlbumController extends Controller
      */
     public function update(UpdateAlbumRequest $request, Album $album)
     {
-        //
+        $album->update($request->all());
+
+        return $album;
     }
 
     /**
@@ -61,6 +63,8 @@ class AlbumController extends Controller
      */
     public function destroy(Album $album)
     {
-        //
+        $album->delete();
+
+        return response('', 204);
     }
 }
