34cd012c74e667b083823cc8a9be68a257373a2a
Tacettin Kustu <tacettinkustu@gmail.com>
2022-02-20 09:22:47 +0300

    Create new controller for image manipulation

--- app/Http/Controllers/V1/AlbumController.php
+++ app/Http/Controllers/V1/AlbumController.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\V1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreAlbumRequest;
 use App\Http\Requests\UpdateAlbumRequest;
 use App\Models\Album;
 
 class AlbumController extends Controller
+
 {
     /**
      * Display a listing of the resource.
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\V1\AlbumController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
