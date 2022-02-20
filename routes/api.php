1a8095edaa22bb08455015016c7d1106b04f317c
Tacettin Kustu <tacettinkustu@gmail.com>
2022-02-20 17:34:59 +0300

    Create dashboardController and dashboard blade files

--- routes/api.php
+++ routes/api.php
@@ -22,10 +22,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('album', AlbumController::class);
-    Route::get('image', [ImageManipulationController::class, 'index']);
-    Route::get('image/by-album/{album}', [ImageManipulationController::class, 'byAlbum']);
-    Route::get('image/{image}', [ImageManipulationController::class, 'show']);
-    Route::post('image/resize', [ImageManipulationController::class, 'resize']);
-    Route::delete('image/{image}', [ImageManipulationController::class, 'destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::apiResource('album', AlbumController::class);
+        Route::get('image', [ImageManipulationController::class, 'index']);
+        Route::get('image/by-album/{album}', [ImageManipulationController::class, 'getByAlbum']);
+        Route::post('image/resize', [ImageManipulationController::class, 'resize']);
+        Route::delete('image/{image}', [ImageManipulationController::class, 'destroy']);
+    });
 });
